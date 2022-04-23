d2c52239551c34277fd02da3a130f9e4f68c3247
Jeong ashely <jashely@naver.com>
2022-04-23 15:23:44 +0900

    get, post request_method통신해보기

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,7 @@
+from django.http.response import JsonResponse
 from shortener.models import Users
 from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -14,6 +16,17 @@ def index(request):
     print(email)
     return render(request, "base.html", {"welcome_msg": f"Hello {email}!"})
 
-def redirect_test(request):
-    print("Go Redirect")
-    return redirect("index")
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+            return JsonResponse(status=201, data=dict(msg="You Just reached with Post Method!"), safe=False)
+    
