2440580f655a4f9bde83dd4f83fa162b6c01c247
jashely <jahsely@mencoz.com>
2022-04-24 19:33:33 +0900

    회원가입 기능추가(django fast form)

--- shortener/models.py
+++ shortener/models.py
@@ -11,9 +11,10 @@ class PayPlan(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
 
 class Users(AbstractUser):
+    full_name = models.CharField(max_length=100, null=True)
     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True)
 
 
-class UserDetail(models.Model):
-    user = models.OneToOneField(Users, on_delete=models.CASCADE)
-    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+# class UserDetail(models.Model):
+#     user = models.OneToOneField(Users, on_delete=models.CASCADE)
+#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from shortener.views import index, get_user
+import debug_toolbar
+from django.conf.urls import include
+from shortener.views import index, get_user, register
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__debug__/", include(debug_toolbar.urls)),
     path("", index, name="index"),
+    path("register", register, name="register"),
     path("get_user/<int:user_id>", get_user),
 ]
